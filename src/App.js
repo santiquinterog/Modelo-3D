79cc3c54846a17bc2598f22a4abd0e97685579e1
santiquinterog <santiago990101@hotmail.com>
2022-10-20 13:00:30 -0500

    Modelo hecho

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { Canvas } from "react-three-fiber";
+import { Suspense } from "react";
+import { OrbitControls, Sky } from "@react-three/drei";
 
+import House from "./components/House/House";
+
+import "./app.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>¡Medieval House!</h1>
+      <div className="div-container-modelo">
+        <Canvas camera={{ zoom: 1, position: [20, 15, 0] }}>
+          <ambientLight intensity={0.5} />
+          <pointLight position={[35,35,0]} intensity={0.4}/>
+          <pointLight position={[-35,35,0]} intensity={0.4}/>
+          <Sky distance={450000} sunPosition={[0, 1, 0]} inclination={0} azimuth={0.25} />
+          <Suspense fallback={null}>
+            <House />
+          </Suspense>
+          <OrbitControls />
+        </Canvas>
+      </div>
+    </>
   );
 }
 
